c0a1674b39168e9d1b3846b03075a8ace596509b
nntha <nnthanh24012003@gmail.com>
2026-01-04 20:00:14 +0700

    trang chu

--- src/Layout/HomeLayout/index.tsx
+++ src/Layout/HomeLayout/index.tsx
@@ -2,7 +2,6 @@ import HeadHome from "./HeadHome.tsx";
 import FooterHome from "./FooterHome";
 import Banner from "./Banner.tsx";
 import HotEvent from "../../components/HotEvent.tsx";
-import CateBreadcrumb from "./CateBreadcrumb.tsx";
 
 const HomeLayout = () =>{
     return (
@@ -10,7 +9,6 @@ const HomeLayout = () =>{
             <HeadHome/>
             <Banner/>
             <HotEvent/>
-            <CateBreadcrumb/>
             <FooterHome/>
         </>
     )
