9ff70a7f625bf7892721bb63b1cd7922f8be5560
nntha <nnthanh24012003@gmail.com>
2026-01-04 23:50:34 +0700

    trang chu

---
+++ src/components/Story.tsx
@@ -0,0 +1,12 @@
+const Story = () =>{
+    return (
+        <>
+            <div className={`flex gap-4 flex-col` }>
+                <h2 className={`text-xl font-bold text-[#4e4e4e]`}>Trả lại vai trò cho bộ môn</h2>
+                <p className={`text-[#7d7d7d] italic`}>8 giờ trước</p>
+                <p className={`text-[#4e4e4e] w-65`}>GD&TĐ - Theo các chuyên gia, việc xác định bộ môn trong trường đại học là đơn vị học thuật, thay vì đơn vị hành chính, sẽ góp phần giảm tải thủ tục và nâng cao chất lượng đào tạo.</p>
+            </div>
+        </>
+    )
+}
+export default Story;
