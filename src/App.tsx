45578078ac3c707ac9c3742a9661028b321d10d5
Pham-Hai-Duong <21130033@st.hcmuaf.edu.vn>
2026-01-06 23:10:28 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
-import {Toaster,} from 'sonner' // hiên thị thông báo
-import {BrowserRouter, Routes, Route} from 'react-router'
-import HomeLayout from "./Layout/HomeLayout"
+import { Toaster } from "sonner";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import HomeLayout from "./Layout/HomeLayout";
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import Category from "./pages/Category";
 
 function App() {
+    return (
+        <>
+            <Toaster />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomeLayout />}>
+                        {/* Trang chủ */}
+                        <Route index element={<Home />} />
 
-  return (
-      <>
-          <Toaster/>
-          <BrowserRouter>
-              <Routes>
-                  <Route path="/"
-                         element={<HomeLayout/>}
-                  />
-              </Routes>
-          </BrowserRouter>
-      </>
-  )
+                        {/* Trang chi tiết */}
+                        <Route path="detail" element={<Detail />} />
+
+                        {/* Trang danh mục */}
+                        <Route path="category/:slug" element={<Category />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
-export default App
+export default App;
